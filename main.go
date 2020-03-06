629d57dbf9116cdf0eff25ea6fae4c911144c4eb
Markko Paas <markko.paas@gmail.com>
2020-03-06 23:53:55 +0100

    add templating for generating redis import statements

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"text/template"
 )
 
 func main() {
+	templatePointer := flag.String("template", "", "Output processing template. Default is json")
+	flag.Parse()
+	var tmpl *template.Template
+	if templatePointer != nil && *templatePointer != "" {
+		var err error
+		tmpl, err = template.New("root").Option("missingkey=error").Parse(*templatePointer)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	r := csv.NewReader(os.Stdin)
 	r.Comma = ';'
 
@@ -27,11 +40,27 @@ func main() {
 
 		lineNumber++
 		if lineNumber == 1 {
-			fieldNames = values // use first line as field names
+			fieldNames = values // use the first line as field names
 		} else {
-			recordMap := zipArrays(fieldNames, values)
-			recordMapJson := toJson(recordMap)
-			fmt.Println(recordMapJson)
+			record := zipArrays(fieldNames, values)
+			recordMapJson := toJson(record)
+			if tmpl == nil {
+				fmt.Println(recordMapJson)
+			} else {
+				templateData := struct {
+					Record     map[string]interface{}
+					RecordJson string
+					LineNumber int
+				}{
+					record,
+					recordMapJson,
+					lineNumber,
+				}
+				err = tmpl.Execute(os.Stdout, templateData)
+				if err != nil {
+					panic(err)
+				}
+			}
 		}
 	}
 }
---
+++ run-redis.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+go run main.go -template "$(< redis.template)" <example.csv
